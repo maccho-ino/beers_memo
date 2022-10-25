625ef487e34b7fe46621cdbcf4e6a40c2a20ef5d
Ayaka Ino <maccho0723@gmail.com>
2022-10-25 08:24:04 +0000

    TOP

--- resources/views/user/mypage.blade.php
+++ resources/views/user/mypage.blade.php
@@ -62,9 +62,9 @@
                             <div class="card-top">
                             </div>
                             @if($post->image_path)
-                            <img class="card-img-top mt-1" src="{{ secure_asset('storage/mymemo_images/' . $post->image_path) }}" alt="Card image">
+                            <img class="card-img-top mt-1" src="{{ asset('storage/mymemo_images/' . $post->image_path) }}" alt="Card image">
                             @else
-                            <img class="card-img-top mt-1" src="{{ secure_asset('/image/noimage_beer.jpg') }}" alt="Card image">
+                            <img class="card-img-top mt-1" src="{{ asset('/image/noimage_beer.jpg') }}" alt="Card image">
                             @endif
                             <p class="card-text">
                             <h5 class="card-title text-light mt-2">{{ Str::limit($post->name, 16) }}</h5>
